7a5c519e6fc7ca6728246fe5c52a00aac8d1ecb3
Xiechimon <xiechimon@qq.com>
2025-01-24 16:17:31 +0800

    Add grain background.

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,15 @@
 import memojiImage from "@/assets/images/memoji-computer.png";
 import Image from "next/image";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
+import grainImage from "@/assets/images/grain.jpg";
 
 export const HeroSection = () => {
   return (
-    <div className="py-32 md:py-48 lg:py-60">
+    <div className="py-32 md:py-48 lg:py-60 relative z-0">
+      <div
+        className="absolute inset-0 -z-30 opacity-5"
+        style={{ backgroundImage: `url(${grainImage.src})` }}
+      ></div>
       <div className="container">
         <div className="flex flex-col items-center">
           <Image
