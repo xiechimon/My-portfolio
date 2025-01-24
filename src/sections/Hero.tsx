9bdbc9288e4743f187baefb7f01731e1dec152c0
Xiechimon <xiechimon@qq.com>
2025-01-24 16:31:37 +0800

    Add rings on top of that grain pattern.

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -5,11 +5,19 @@ import grainImage from "@/assets/images/grain.jpg";
 
 export const HeroSection = () => {
   return (
-    <div className="py-32 md:py-48 lg:py-60 relative z-0">
+    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+      {/* Grain background */}
       <div
         className="absolute inset-0 -z-30 opacity-5"
         style={{ backgroundImage: `url(${grainImage.src})` }}
       ></div>
+
+      <div className="size-[620px] hero-ring"></div>
+      <div className="size-[820px] hero-ring"></div>
+      <div className="size-[1020px] hero-ring"></div>
+      <div className="size-[1220px] hero-ring"></div>
+
+      {/* Hero content */}
       <div className="container">
         <div className="flex flex-col items-center">
           <Image
