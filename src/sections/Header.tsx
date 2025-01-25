74fba6942081a84a880043233d13891c5ca4e887
Xiechimon <xiechimon@qq.com>
2025-01-25 20:23:20 +0800

    Add Featured projects section.

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -3,16 +3,19 @@ export const Header = () => {
     <div className="flex justify-center items-center fixed top-3 w-full z-10">
       <nav className="flex gap-1 p-0.5 border-white/15 rounded-full bg-white/10 backdrop-blur">
         <a href="#" className="nav-item">
-          Home
+          首页
         </a>
         <a href="#" className="nav-item">
-          Projects
+          项目
         </a>
         <a href="#" className="nav-item">
-          About
+          关于
         </a>
-        <a href="#" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">
-          Contact
+        <a
+          href="#"
+          className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
+        >
+          联系
         </a>
       </nav>
     </div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -67,28 +67,25 @@ export const HeroSection = () => {
           />
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
             <div className="bg-green-500 size-2.5 rounded-full"></div>
-            <div className="text-sm font-medium">
-              Available for new projects
-            </div>
+            <div className="text-sm font-medium">摸鱼中</div>
           </div>
         </div>
         <div className="max-w-lg mx-auto">
           <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
-            Hi, I'm Xiechimon. A developer.
+            Hi, I'm Xiechimon. A developer
           </h1>
           <p className="mt-4 text-center text-white/60 md:text-lg">
-            I'm a full-stack developer with a passion for building beautiful and
-            functional websites and applications.
+            “我正在学习全栈开发，热衷于构建既美观又实用的网站和应用程序。”
           </p>
         </div>
         <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
           <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
-            <span className="font-semibold">Explore My Work</span>
+            <span className="font-semibold">浏览我的作品</span>
             <ArrowDown className="size-4" />
           </button>
           <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
             <span>👋</span>
-            <span className="font-semibold">Let's Connect</span>
+            <span className="font-semibold">联系我们</span>
           </button>
         </div>
       </div>
